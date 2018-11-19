9d15df729351e83bdd0b44661f869785aae8f803
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-19 19:47:10 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,17 @@
 module Memorable
-  def reset_all
-    all.clear
+  module ClassMethods
+    def reset_all
+      all.clear
+    end
+
+    def count
+      all.count
+    end
   end
 
-  def count
-    all.count
+  module InstanceMethods
+    def initialize
+      all << self
+    end
   end
 end
